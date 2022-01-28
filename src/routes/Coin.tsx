490fe2a400bb2e997ee576475c763d5ea4926800
국산자몽 <50566360+K-Grapefruit@users.noreply.github.com>
2022-01-28 18:39:04 +0900

    11

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -233,7 +233,7 @@ function Coin({}: ICoinProps) {
             </OverviewItem>
             <OverviewItem>
               <span>Price:</span>
-              <span>${tickersData?.quotes.USD.price.toFixed(3)}</span>
+              <span>${tickersData?.quotes?.USD.price.toFixed(3)}</span>
             </OverviewItem>
           </Overview>
           <Description>{infoData?.description}</Description>
